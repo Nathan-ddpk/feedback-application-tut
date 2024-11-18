12e02e91ddb31b3443f32ff895fbbecd818d0fac
Nathan-ddpk <135796134+Nathan-ddpk@users.noreply.github.com>
2024-11-18 11:32:01 +0000

    Feature: Add basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World...............")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Welcome to Server Desk API" })
+})
+
+app.listen(PORT, () => console.log(`Listening the server in port: ${PORT}`))
