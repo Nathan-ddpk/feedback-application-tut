4da6fc8dc49dc78e042c5aba9dfd47cab70c8783
Nathan-ddpk <135796134+Nathan-ddpk@users.noreply.github.com>
2024-11-19 09:39:57 +0000

    Feature: Add userController and errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middlewares/errorMiddleware')
+
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to Server Desk API" })
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Listening the server in port: ${PORT}`))
